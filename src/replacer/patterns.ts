e1b489b3133ada5a9d79313aaf89f264779ad68d
TechiePi <techie@techiepi.dev>
2022-01-20 22:18:29 +0100

    feat: add bcrypt regex

--- src/replacer/patterns.ts
+++ src/replacer/patterns.ts
@@ -2,13 +2,12 @@ import ipRegex from "ip-regex";
 
 const isIpAddress = ipRegex({ exact: false });
 const isPhoneNumber = /^(\+*)(\d*)([(]\d{1,3}[)]*)*(\s?\d+|\+\d{2,3}\s\d+|\d+)[\s|-]?\d+([\s|-]?\d+){1,2}(\s)*$/gm;
+const isBcryptHash = /^\$2[ayb]\$.{56}$/g;
 
-export {
+const _patterns = {
     isIpAddress,
     isPhoneNumber,
+    isBcryptHash,
 }
 
-export default [
-    isIpAddress,
-    isPhoneNumber,
-]
+export default Object.values(_patterns);
