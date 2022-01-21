919b4fc29acefcf77515d6367c9131d982799b86
TechiePi <techie@techiepi.dev>
2022-01-21 10:35:12 +0100

    fix: github token pattern not working

--- src/replacer/patterns.ts
+++ src/replacer/patterns.ts
@@ -3,7 +3,7 @@ import ipRegex from "ip-regex";
 const isIpAddress = ipRegex({ exact: false });
 const isPhoneNumber = /^(\+*)(\d*)([(]\d{1,3}[)]*)*(\s?\d+|\+\d{2,3}\s\d+|\d+)[\s|-]?\d+([\s|-]?\d+){1,2}(\s)*$/gm;
 const isBcryptHash = /^\$2[ayb]\$.{56}$/g;
-const isGitHubToken = /((gh._)(.*)\s+)/;
+const isGitHubToken = /((gh._)(.*)\s+)/g;
 
 const _patterns = {
     isIpAddress,
