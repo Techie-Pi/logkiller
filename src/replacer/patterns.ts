dd2e93573dae2706e4dfcfa228f44a70f131a265
TechiePi <techie@techiepi.dev>
2022-01-21 11:31:12 +0100

    chore: improve bcrypt regex

--- src/replacer/patterns.ts
+++ src/replacer/patterns.ts
@@ -1,7 +1,7 @@
 import ipRegex from "ip-regex";
 
 const isIpAddress = ipRegex({ exact: false });
-const isBcryptHash = /(\$2a\$|\$2b\$|\$2x\$|\$2y\$)(.*)\s+/gi;
+const isBcryptHash = /(\$2a\$|\$2b\$|\$2x\$|\$2y\$)(.*)(\s+|[\n|\r\n|\r]{2})/gi;
 const isScryptHash = /(c2NyeXB0)(.*)(\s+|[\n|\r\n|\r]{2})/gi;
 const isArgon2Hash = /(\$argon2)(.*)(\s+|[\n|\r\n|\r]{2})/gi;
 const isGitHubToken = /((gh._)(.*)\s+)/g;
