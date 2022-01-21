a9e7f8201b7684948038f9780630dcb2343f5e4f
TechiePi <techie@techiepi.dev>
2022-01-21 11:30:52 +0100

    feat: add scrypt and argon2 hashes

--- src/replacer/patterns.ts
+++ src/replacer/patterns.ts
@@ -2,11 +2,15 @@ import ipRegex from "ip-regex";
 
 const isIpAddress = ipRegex({ exact: false });
 const isBcryptHash = /(\$2a\$|\$2b\$|\$2x\$|\$2y\$)(.*)\s+/gi;
+const isScryptHash = /(c2NyeXB0)(.*)(\s+|[\n|\r\n|\r]{2})/gi;
+const isArgon2Hash = /(\$argon2)(.*)(\s+|[\n|\r\n|\r]{2})/gi;
 const isGitHubToken = /((gh._)(.*)\s+)/g;
 
 const _patterns = {
     isIpAddress,
     isBcryptHash,
+    isScryptHash,
+    isArgon2Hash,
     isGitHubToken,
 }
 
