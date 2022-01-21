a34b8e4e1a781495bc294d055d38b7a8ff2e90e2
TechiePi <techie@techiepi.dev>
2022-01-21 11:22:59 +0100

    fix: bcrypt hash and remove phone number

--- src/replacer/patterns.ts
+++ src/replacer/patterns.ts
@@ -1,13 +1,11 @@
 import ipRegex from "ip-regex";
 
 const isIpAddress = ipRegex({ exact: false });
-const isPhoneNumber = /^(\+*)(\d*)([(]\d{1,3}[)]*)*(\s?\d+|\+\d{2,3}\s\d+|\d+)[\s|-]?\d+([\s|-]?\d+){1,2}(\s)*$/gm;
-const isBcryptHash = /^\$2[ayb]\$.{56}$/g;
+const isBcryptHash = /(\$2a\$|\$2b\$|\$2x\$|\$2y\$)(.*)\s+/gi;
 const isGitHubToken = /((gh._)(.*)\s+)/g;
 
 const _patterns = {
     isIpAddress,
-    isPhoneNumber,
     isBcryptHash,
     isGitHubToken,
 }
